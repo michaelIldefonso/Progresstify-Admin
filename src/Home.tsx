2b00072c79c41b820659337dc0acf2dfeeb5d925
Michael Ildefonso <michaelildefonso8@gmail.com>
2025-03-30 23:25:20 +0800

    decided to use typescript

--- src/Home.tsx
+++ src/Home.tsx
@@ -7,8 +7,10 @@ function Home() {
             <Navbar /> {/* Use Navbar component */}
             <div className="flex-1 p-4">
                 <div className="container mx-auto mt-8">
-                    <h1>Welcome to Progresstify</h1>
-                    <p>Your one-stop solution for progress tracking and management.</p>
+                    <h1>Welcome NAME</h1>
+                    <p>Power: ADMIN</p>
+                    <p></p>
+
                 </div>
             </div>
         </div>
